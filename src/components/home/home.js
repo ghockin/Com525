1ccb3d4120405fa07f9260af5193757a7fe31e5c
ghockin <79321882+ghockin@users.noreply.github.com>
2022-06-30 11:43:22 +0100

    Homepage styling Complete

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -3,8 +3,129 @@ import './home.css';
 
 const Home = () => {
   return (
-    <div className="App">
-      <header><h1>Interaction Design Components</h1> <h2>Gabe Hockin</h2></header>
+    <div className="home">
+      <div className="about">
+        <h2> Interaction Design</h2>
+        <br />
+        <br />
+        <h2> Gabe Hockin</h2>
+      </div>
+
+      {/* Login form */}
+      <div className="skills">
+        <h1> Login Form</h1>
+        <ol className="list">
+          <li className="item">
+            <h2> Research</h2>
+            <span>
+              Research into Component
+            </span>
+          </li>
+          <li className="item">
+            <h2>Design</h2>
+            <span>
+              Figma Design
+            </span>
+          </li>
+          <li className="item">
+            <h2>Prototype</h2>
+            <span>Completed Component</span>
+          </li>
+        </ol>
+      </div>
+
+      {/* Checkbox */}
+      <div className="skills">
+        <h1> Checkbox</h1>
+        <ol className="list">
+          <li className="item">
+            <h2> Research</h2>
+            <span>
+              Research into Component
+            </span>
+          </li>
+          <li className="item">
+            <h2>Design</h2>
+            <span>
+              Figma Design
+            </span>
+          </li>
+          <li className="item">
+            <h2>Prototype</h2>
+            <span>Completed Component</span>
+          </li>
+        </ol>
+      </div>
+
+      {/* Image Slider */}
+      <div className="skills">
+        <h1> Image Slider</h1>
+        <ol className="list">
+          <li className="item">
+            <h2> Research</h2>
+            <span>
+              Research into Component
+            </span>
+          </li>
+          <li className="item">
+            <h2>Design</h2>
+            <span>
+              Figma Design
+            </span>
+          </li>
+          <li className="item">
+            <h2>Prototype</h2>
+            <span>Completed Component</span>
+          </li>
+        </ol>
+      </div>
+
+      {/* Modal */}
+      <div className="skills">
+        <h1> Modal</h1>
+        <ol className="list">
+          <li className="item">
+            <h2> Research</h2>
+            <span>
+              Research into Component
+            </span>
+          </li>
+          <li className="item">
+            <h2>Design</h2>
+            <span>
+              Figma Design
+            </span>
+          </li>
+          <li className="item">
+            <h2>Prototype</h2>
+            <span>Completed Component</span>
+          </li>
+        </ol>
+      </div>
+
+      {/* NavBar */}
+      <div className="skills">
+        <h1> Navigation Bar</h1>
+        <ol className="list">
+          <li className="item">
+            <h2> Research</h2>
+            <span>
+              Research into Component
+            </span>
+          </li>
+          <li className="item">
+            <h2>Design</h2>
+            <span>
+              Figma Design
+            </span>
+          </li>
+          <li className="item">
+            <h2>Prototype</h2>
+            <span>Completed Component</span>
+          </li>
+        </ol>
+      </div>
+
     </div>
   );
 }
